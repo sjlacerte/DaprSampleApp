55d186b9bec00cd66fe9a5430a0d64f25b8a4371
Stephen Lacerte <stephenlacerte@gmail.com>
2026-05-26 22:09:10 -0700

    feat: add Terraform EKS cluster (t3.small x2)

---
+++ terraform/eks.tf
@@ -0,0 +1,26 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.30"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size       = 2
+      max_size       = 2
+      desired_size   = 2
+      instance_types = ["t3.small"]
+
+      labels = local.common_tags
+    }
+  }
+
+  tags = local.common_tags
+}
